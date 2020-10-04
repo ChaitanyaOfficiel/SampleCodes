515657f40221c83084717a14c6eab64b632498f8
Chaitanya <VankarChaitanya@outlook.com>
2020-10-04 22:32:20 +0530

    Add files via upload

---
+++ passemot by while loop.py
@@ -0,0 +1,11 @@
+
+password = 12345
+ 
+while True :
+    inputed_number = input('Enter the Password: ')
+
+    if inputed_number == password:
+        print("Acess granted")
+    else:
+        print("Acess Deined")
+    
---
+++ passeword.py
@@ -0,0 +1,7 @@
+password = "Password8274"
+inputed_Password = input("Enter the password:")
+if inputed_Password == password:
+    print("Acess granted")
+else:
+    print("Acess Deined")
+
